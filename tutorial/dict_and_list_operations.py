029fff37aeac0c1f48096ec24755fe7b6abfad68
Bennett Bullock <bennettbullock@Bennetts-MacBook-Pro.local>
2025-08-11 16:35:05 -0400

    First commit

---
+++ tutorial/dict_and_list_operations.py
@@ -0,0 +1,212 @@
+'''
+python3 dict_and_list_operations.py 
+
+python3 watch_file.py -p2 python3 dict_and_list_operations.py -p1 ./reinstall_from_source.sh -d /Users/bennettbullock/python-pype-lang-3
+'''
+from pype3 import pypeify,pypeify_namespace,p,_,_0,_1,_2,_last,c
+from pype3 import ep,db,a,iff,d,ift,squash,ifp,m,change,app,l
+from pype3.time_helpers import *
+from pype3.helpers import *
+from pype3.vals import PypeVal as v
+from copy import deepcopy
+from pype3.numpy_helpers import *
+import numpy
+'''
+Basic tutorial that covers switch dict/JSON manipulations.
+'''
+
+###############
+# DICT BUILDS #
+###############
+
+def dict_build_simple_1(n):
+    '''
+    This shows a simple way to build a dictionary, and then add another key
+    to it. db('n') takes the accum and builds a singleton dictionary keyed by
+    'n'. a('m',_.n+3) adds another key-value pair to the accum, where the
+    key is 'm' and the value is the value for 'n' plus 3.
+    '''
+    (db('n'), # {'n':1}
+     a('m',_.n+3), # {'n':1,'m':4}
+    )
+
+
+def dict_build_merge(n,d2={'a':1,'b':2}):
+    '''
+    This will show how pype can merge dictionaries together in a simple
+    and elegant manner. m(d2) merges the accum with d2, replacing any 
+    equally-keyed values in the accum with their values in d2.
+    '''
+    (db('a'), # {'a':2}
+     m(d2), # {'a':1,'b':2}
+    )
+
+
+def dict_build_dissoc(n):
+    '''
+    Here, we show how to delete a key-value pair from the dict.
+    '''
+    (db('a'), # {'a':2}
+     a('b',_.a+3), # {'a':2,'b':4}
+     a('c',_.a*_.b), # {'a':2,'b':4,'c':8}
+     d('b'), # {'a':2,'c':8} - we drop 'b' from the dictionary.
+    )
+
+
+def dict_build_explicit(n):
+    '''
+    Here, we are doing an explicit dict build.  This differs from a switch
+    dict by not having 'else' as a key.  
+    '''
+    ({'a':_,
+      'b':_+2,
+      'c':_*5},
+    )
+
+
+def dict_build_squash(n):
+    '''
+    The squash function takes an embedded dictionary, and makes its key-value
+    pairs part of the embedding dictionary.
+    '''
+    ({'a':_,
+      'b':{'d':_+2,
+           'e':_*20},
+      'c':_*5}, # ('a':2,'b':{'d':4,'e':40},'c':10}
+     squash('b'), # ('a':2,'d':4,'e':40,'c':10}
+    )
+
+
+def dict_build_change(n):
+    '''
+    The change function changes the key in the dicitonary.
+    '''
+    (db('a'), # {'a':2}
+     change('a','b'), # {'b':2}
+    )
+
+
+def dict_build_std(ls):
+    '''
+    In this example we compute the median standard deviation.  For a varable
+    x, this would be median(|x_i - median(x)|).  
+
+    This also shows how easily pype3 is integrated with numpy.  Notice the
+    subtraction of two numpy arrays in _.a - _.median.
+    '''
+    (np.array, # cast list of Python integers to numpy array.
+     {'a':_, # 'a' stores the array.
+      'median':np.median}, # we compute the median
+     _.a - _.median, # take the difference from the median
+     np.abs, # take the absolute values.
+     np.median, # take the median of that
+    )
+
+
+###############
+# LIST BUILDS #
+###############
+
+def simple_ls_build(a,b):
+    '''
+    This demonstrates the list build function l, which creates a list of
+    its arguments.
+    '''
+    (l(a,b),
+    )
+
+
+def dct_ls_build(js):
+    '''
+    This extracts key-value pairs from a dictionary and builds tuples with
+    transformations on both the key and the pair.
+    '''
+    (dct_items, # Gets a list of key-value tuples.
+     [l('s'+_0,_1*100)], # Prepend 's' to the key, and multiply value by 100
+    )
+
+
+def append_ls(ls2,el):
+    '''
+    This adds an element to the end of the list.
+    '''
+    (app(el),
+    )
+
+
+def concat_ls(ls1,ls2):
+    '''
+    This concatenates both lists.
+    '''
+    (c(_,ls2),
+    )
+
+
+pypeify_namespace(globals())
+
+if __name__=='__main__':
+
+    print('*'*30)
+    print('dict builds')
+    print('*'*30)
+
+    print('dict_build_simple_1(1)')
+    print(dict_build_simple_1(1))
+
+    print('dict_build_merge(5)')
+    print(dict_build_merge(5))
+
+    print('dict_build_dissoc(2)')
+    print(dict_build_dissoc(2))
+
+    print('dict_build_explicit(2)')
+    print(dict_build_explicit(2))
+
+    print('dict_build_squash(2)')
+    print(dict_build_squash(2))
+
+    print('dict_build_change(2)')
+    print(dict_build_change(2))
+
+    ls=[1,2,5,3,5,6,6,4,3,66,7,8,4,3,2,3,5,68,7,5,43,2,2,4,565,3,2,2222,22,4]
+
+    print('-'*30)
+    print(f'{ls} is ls')
+    print('-'*30)
+
+    print(dict_build_std(ls))
+    print(dict_build_std(ls))
+
+    print('*'*30)
+    print('list builds')
+    print('*'*30)
+
+    print('simple_ls_build(1,2)')
+    print(simple_ls_build(1,2))
+
+    js={'a':1,'b':2,'c':3,'d':4}
+
+    print('-'*30)
+    print(f'{js} is js')
+    print('-'*30)
+
+    print('dct_ls_build(js)')
+    print(dct_ls_build(js))
+
+    ls=ls[:10]
+
+    print('-'*30)
+    print(f'{ls} is ls')
+    print('-'*30)
+
+    print(append_ls(ls,1))
+
+    ls1=[1,2,3,5,3]
+    ls2=[1,2,3,3]
+
+    print('-'*30)
+    print(f'{ls1} is ls1')
+    print(f'{ls2} is ls2')
+    print('-'*30)
+
+    print(concat_ls(ls1,ls2))
---
+++ tutorial/switch_dicts_and_embedded_pypes.py
@@ -0,0 +1,230 @@
+'''
+python3 switch_dicts_and_embedded_pypes.py 
+
+python3 watch_file.py -p2 python3 switch_dicts_and_embedded_pypes.py -p1 ./reinstall_from_source.sh -d /Users/bennettbullock/python-pype-lang-3
+'''
+from pype3 import pypeify,pypeify_namespace,p,_,_0,_1,_2,_last
+from pype3 import ep,db,a,iff,d,ift,iftp,squash,ifp
+from pype3.time_helpers import *
+from pype3.helpers import *
+from pype3.vals import PypeVal as v
+from copy import deepcopy
+from pype3.numpy_helpers import *
+
+'''
+Basic tutorial that covers switch dicts and embedded pypes.  
+
+Switch dicts these implement conditional statements.  Embedded pypes do a 
+separate evaluation on the accum.
+'''
+
+################
+# SWITCH DICTS #
+################
+
+def switch_dict(n):
+    '''
+    This is shows the simple functionality of a switch-dict.  If n is equal to
+    any key in the switch dict, we evaluate the corresponding fArg value, and
+    return it.
+    '''
+    ({_ == 1:'one',
+      _ == 2:'two',
+      'else':"I don't know"},
+    )
+
+
+def iff_example(n):
+    '''
+    The iff macro returns n*100 if n>2, else n.
+    '''
+    (iff(_>2,_*100),
+    )
+
+
+def ift_example(n):
+    '''
+    The iff macro returns n*100 if n>2, else False.
+    '''
+    (ift(_>2,_*100),
+    )
+    
+
+def func(x,k):
+    '''
+    This is just a simple polynomial x**2 + x + k. 
+    '''
+    (_ + _**2 + k,
+    )
+
+
+def sm(a,b):
+
+    (_+b,
+    )
+
+
+def embedded_pype(ls):
+    '''
+    An embedded pype, or 'ep' takes the accum, and evaluates a sequence of
+    fArgs on it.  The ep in this case evaluates:
+
+    func(max(ls)*2,4)
+    '''
+    ((sm,len,ep(max,
+                _*2,
+                (func,_,4))),
+    )
+
+
+def embedded_pype_dict(js):
+    '''
+    An embedded pype, or 'ep' takes the accum, and evaluates a sequence of
+    fArgs on it.  The ep in this case evaluates:
+
+    func(max(dct_values(js))*2,4)
+    '''
+    ((sm,len,ep(dct_values,
+                max,
+                _*2,
+                (func,_,4))),
+    )
+
+
+def switch_dict_and_ep(ls):
+    '''
+    Here we are going to do some more sophisticated computation.  If the
+    ls has a length of less than 3, we apply an 'ep', or 'embedded pype',
+    shown in the last function.  Otherwise, we return the length of the list. 
+    '''
+    ({len < 3:ep(max,
+                 _*2,
+                 (func,_,4)),
+      'else':len},
+    )
+
+
+def ift_example(n):
+    '''
+    The first argument of an ifp statement is a contitional, and the rest 
+    are fArgs for an ep. If the conditional evaluates as True, return the
+    results of the ep.  Otherwise, return n.
+    '''
+    (ift(_ > 2,_*100),
+    )
+
+
+def iftp_example(n):
+    '''
+    The first argument of an iftp statement is a contitional, and the rest 
+    are fArgs for an ep. If the conditional evaluates as True, return the
+    results of the ep.  Otherwise, return False.
+    '''
+
+    (ifp(_*10,[_+1]),
+    )
+
+
+def ifp_example(n):
+    '''
+    If the accum is n, evaluate the accum in the fArgs.  Otherwise, 
+    return n.
+    '''
+    (ifp(_*10,[_+1]),
+    )
+
+
+pypeify_namespace(globals())
+
+if __name__=='__main__':
+
+    print('*'*30)
+    print('switch dicts')
+    print('*'*30)
+
+    print('switch_dict(1)')
+    print(switch_dict(1))
+
+    print('switch_dict(2)')
+    print(switch_dict(2))
+
+    print('switch_dict(3)')
+    print(switch_dict(3))
+
+    print('iff_example(1)')
+    print(iff_example(1))
+
+    print('iff_example(3)')
+    print(iff_example(3))
+
+    print('ift_example(1)')
+    print(ift_example(1))
+
+    print('ift_example(3)')
+    print(ift_example(3))
+    
+    print('*'*30)
+    print('embedded pypes for lists')
+    print('*'*30)
+
+    ls=[-2,-3,5,-1,-1,2,3,4]
+
+    print('-'*30)
+    print(f'{ls} is ls')
+    print('-'*30)
+
+    print('embedded_pype(ls)')
+    print(embedded_pype(ls))
+
+    print('*'*30)
+    print('embedded pypes for dicts')
+    print('*'*30)
+
+    js={'a':-2,'b':-3,'c':5,'d':-1,'e':-1,'f':2,'g':3,'h':4}
+
+    print('-'*30)
+    print(f'{js} is js')
+    print('-'*30)
+
+    print('embedded_pype_dict(js)')
+    print(embedded_pype_dict(js))
+
+    print('*'*30)
+    print('switch dict and embedded pype')
+    print('*'*30)
+    
+    ls=[-2,-3,5,-1,-1,2,3,4]
+
+    print('-'*30)
+    print(f'{ls} is ls')
+    print('-'*30)
+
+    print('switch_dict_and_ep(ls)')
+    print(switch_dict_and_ep(ls))
+
+    ls=[-2,-3]
+
+    print('-'*30)
+    print(f'{ls} is ls')
+    print('-'*30)
+
+    print('switch_dict_and_ep(ls)')
+    print(switch_dict_and_ep(ls))
+
+    print('ift_example(1)')
+    print(ift_example(1))
+
+    print('ift_example(5)')
+    print(ift_example(5))
+
+    print('ifp_example([])')
+    print(ifp_example([]))
+
+    print('ifp_example([1,2,3,4])')
+    print(ifp_example([1,2,3,4]))
+
+    print('iftp_example([])')
+    print(iftp_example([]))
+
+    print('iftp_example([1,2,3,4])')
+    print(iftp_example([1,2,3,4]))
